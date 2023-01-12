9cb65e46b15dff0b7252644c0f6c0cdeaad57033
imhedhebi <imhedhebi@gaming.bs>
2023-01-12 15:51:20 +0100

    test

--- ajouter.php
+++ ajouter.php
@@ -18,14 +18,47 @@
         </p>
         <form action="" method="POST">
             <label>Nom</label>
-            <input type="text" name="nom">
+            <input type="text" name="nom" id="name">
             <label>Prénom</label>
-            <input type="text" name="prenom">
+            <input type="text" name="prenom" id="firstname">
             <label>âge</label>
-            <input type="number" name="age">
+            <input type="number" name="age" id="age">
             <input type="submit" value="Ajouter" name="button">
         </form>
     </div>
 </body>
 
 </html>
+
+
+<?php
+
+    if(isset($_POST['submit'])){
+
+        $name = $_POST['name'];
+        $firstname = $_POST['firstname']; 
+        $age = $_POST['age']; 
+        
+
+        
+        
+
+        //CHECK DUPLICATE & ADD IN DATABASE
+        $check_duplicate = "SELECT * FROM db_php WHERE name = '$name' AND firstname = '$firstname'";
+        $result_check = mysqli_num_rows(mysqli_query($conn,$check_duplicate));
+
+        if($result_check > 0){
+            echo '<script>alert("Record already existed")</script>';
+        }else{
+            $insert_sql =  "INSERT INTO db_php VALUES ('','$name', '$firstname', '$age')";
+        
+            if (mysqli_query($conn,$insert_sql)){
+                echo '<script>alert("New record created successfully")</script>';
+            }else{
+                echo "Error: " . $insert_sql . "<br>" . mysqli_error($insert_sql);
+            }
+        }
+    }
+        
+        
+?>
